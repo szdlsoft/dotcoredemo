3f29ac89b5c2b1f1f00d0bcbc1005747e8bca7d0
szdls <szdls@LAPTOP-MJVJ39DS>
2017-08-06 08:14:59 +0800

    添加项目文件。

---
+++ DotnetCoreDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
